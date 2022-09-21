fa229ec2249f4ea5165b9e037e8351585976031b
Damir021 <k.damir021@gmail.com>
2022-09-21 15:33:04 +0600

    add slides

--- static/js/main.js
+++ static/js/main.js
@@ -73,6 +73,57 @@ document.querySelectorAll('.mobile-menu .nav__link').forEach(function (item) {
 });
 "use strict";
 
+// document.querySelectorAll("a[href^='#'").forEach(link => {
+//     link.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         let href = this.getAttribute("href").substring(1);
+//         const scrollTarget = document.getElementById(href);
+//         if(!scrollTarget) return;
+//         const topOffset = document.querySelector(".header").offsetHeight + 20;
+//         // const topOffset = 0; // если не нужен отступ сверху
+//         const elementPosition = scrollTarget.getBoundingClientRect().top;
+//         const offsetPosition = elementPosition - topOffset;
+//         window.scrollBy({
+//             top: offsetPosition,
+//             behavior: "smooth"
+//         });
+//     });
+// });
+document.querySelectorAll("a").forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    var href = link.hash.substring(1);
+    var scrollTarget = document.getElementById(href);
+
+    if (scrollTarget) {
+      e.preventDefault();
+    } else {
+      return;
+    }
+
+    var topOffset = document.querySelector(".header").offsetHeight + 20;
+    var elementPosition = scrollTarget.getBoundingClientRect().top;
+    var offsetPosition = elementPosition - topOffset;
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  });
+});
+window.addEventListener('scroll', function () {
+  var scrollTop = getBodyScrollTop();
+
+  if (scrollTop > 0) {
+    document.querySelector('.header').classList.add('scrolled');
+  } else {
+    document.querySelector('.header').classList.remove('scrolled');
+  }
+});
+
+function getBodyScrollTop() {
+  return self.pageYOffset || document.documentElement && document.documentElement.scrollTop || document.body && document.body.scrollTop;
+}
+"use strict";
+
 var timerElement = document.querySelector('.timer');
 var partnersBlock = document.getElementById('partners');
 var fixedLinkElement = document.querySelector('.fixed-link');
@@ -127,55 +178,4 @@ if (fixedLinkElement) {
       fixedLinkElement.classList.remove('active');
     }
   });
-}
-"use strict";
-
-// document.querySelectorAll("a[href^='#'").forEach(link => {
-//     link.addEventListener("click", function (e) {
-//         e.preventDefault();
-//         let href = this.getAttribute("href").substring(1);
-//         const scrollTarget = document.getElementById(href);
-//         if(!scrollTarget) return;
-//         const topOffset = document.querySelector(".header").offsetHeight + 20;
-//         // const topOffset = 0; // если не нужен отступ сверху
-//         const elementPosition = scrollTarget.getBoundingClientRect().top;
-//         const offsetPosition = elementPosition - topOffset;
-//         window.scrollBy({
-//             top: offsetPosition,
-//             behavior: "smooth"
-//         });
-//     });
-// });
-document.querySelectorAll("a").forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    var href = link.hash.substring(1);
-    var scrollTarget = document.getElementById(href);
-
-    if (scrollTarget) {
-      e.preventDefault();
-    } else {
-      return;
-    }
-
-    var topOffset = document.querySelector(".header").offsetHeight + 20;
-    var elementPosition = scrollTarget.getBoundingClientRect().top;
-    var offsetPosition = elementPosition - topOffset;
-    window.scrollBy({
-      top: offsetPosition,
-      behavior: "smooth"
-    });
-  });
-});
-window.addEventListener('scroll', function () {
-  var scrollTop = getBodyScrollTop();
-
-  if (scrollTop > 0) {
-    document.querySelector('.header').classList.add('scrolled');
-  } else {
-    document.querySelector('.header').classList.remove('scrolled');
-  }
-});
-
-function getBodyScrollTop() {
-  return self.pageYOffset || document.documentElement && document.documentElement.scrollTop || document.body && document.body.scrollTop;
 }
